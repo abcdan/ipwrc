70c4efac9b192e4e0a774d152eb20e367fa29c72
Daniel <daniel@lngzl.nl>
2020-12-21 18:46:34 +0100

    :sparkles: product

---
+++ backend/database/models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const ProductSchema = mongoose.Schema({
+  slug: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  image: {
+    type: String,
+    required: true
+  },
+  price: {
+    type: Number,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  }
+})
+
+module.exports = mongoose.model('product', ProductSchema)
