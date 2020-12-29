ef033844612b2f1571ae1c5238e6593aaa2f59c7
abcdan <daniel@lngzl.nl>
2020-12-29 22:21:10 +0100

    :sparkles: cart working

--- ipwrc/src/app/cart/cart.component.ts
+++ ipwrc/src/app/cart/cart.component.ts
@@ -8,13 +8,31 @@ import { CartService } from './cart.service';
 })
 export class CartComponent implements OnInit {
 
-  constructor(private cart: CartService) { }
   // TOOD: rename lol, or not. it's funny cause Playboi Carti is an artist
-  playboiCarti: any[] | undefined
+  playboiCarti: any[]
 
-  ngOnInit(): void {
+  constructor(private cart: CartService) { 
     this.playboiCarti = this.cart.getCartItems()
     console.log(this.playboiCarti)
+  }
+
+  ngOnInit(): void {
+  }
+
+  getTotalPrice(): number {
+    let price = 0
+    this.playboiCarti.forEach(item => {
+      price += item.product[0].price 
+    })
+    return price
+  }
+
+  countProducts(): number {
+    let items = 0
+    this.playboiCarti.forEach(item => {
+      items += item.amount
+    })
+    return items
   }
 
 
