ace993bb9ee4979408d77d19fbecf5371b25a641
Daniel <daniel@lngzl.nl>
2021-01-02 23:07:34 +0100

    :sparkles: user data

--- backend/routes/order/fetch.js
+++ backend/routes/order/fetch.js
@@ -8,8 +8,10 @@ module.exports = (app, endpoint) => {
     try {
       const order = await global.models('Order').findById(req.params.orderId)
       if(req.user.id !==  order.userId && !req.user.admin) return res.status(401).json({success: false, message: 'thats not your order'})
-
-      res.json(await order)
+      const user = await global.models('User').findById(order.userId)
+      res.json({
+         user, order
+      })
     } catch (e) {
       res.status(400).json({ success: false, message: 'couldnt fetch order' })
     }
