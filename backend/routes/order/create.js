b8f73273b1dec4a76cf4365616e4cfa39b4c42d3
Daniel <daniel@lngzl.nl>
2021-01-02 21:48:07 +0100

    :sparkles: waht

--- backend/routes/order/create.js
+++ backend/routes/order/create.js
@@ -1,4 +1,5 @@
 const authentication = require('../../middleware/authentication')
+const mongoose = require('mongoose')
 // const OrderProduct = require('../../database/models/OrderProduct')
 
 module.exports = (app, endpoint) => {
@@ -6,6 +7,7 @@ module.exports = (app, endpoint) => {
   * Create a new order
   */
   app.post(endpoint, authentication, async (req, res) => {
+    delete mongoose.connection.models['OrderProduct'];
     const { products } = req.body
     if(products.length === 0) return res.status(400).json({ success: false, message: 'your forgot to give an array of products with id/amount' })
     if (!products || !products.length === 0) { return res.status(400).json({ success: false, message: 'your forgot to give an array of products with id/amount' }) }
