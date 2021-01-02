73e92dcde0fb2bfb8cab441ab382fb283928ebe6
Daniel <daniel@lngzl.nl>
2021-01-02 21:50:51 +0100

    :sparkles: pelase work

--- backend/routes/order/create.js
+++ backend/routes/order/create.js
@@ -1,13 +1,12 @@
 const authentication = require('../../middleware/authentication')
 const mongoose = require('mongoose')
-// const OrderProduct = require('../../database/models/OrderProduct')
+const OrderProduct = require('../../database/models/OrderProduct')
 
 module.exports = (app, endpoint) => {
   /**
   * Create a new order
   */
   app.post(endpoint, authentication, async (req, res) => {
-    delete mongoose.connection.models['OrderProduct'];
     const { products } = req.body
     if(products.length === 0) return res.status(400).json({ success: false, message: 'your forgot to give an array of products with id/amount' })
     if (!products || !products.length === 0) { return res.status(400).json({ success: false, message: 'your forgot to give an array of products with id/amount' }) }
@@ -17,7 +16,7 @@ module.exports = (app, endpoint) => {
         const product = await global.models('Order').findOne({
           slug: products[i].slug
         })
-        const orderProduct = await new global.models('OrderProduct')({
+        const orderProduct = new OrderProduct({
           slug: product.slug,
           name: product.name,
           price: product.price,
@@ -25,7 +24,7 @@ module.exports = (app, endpoint) => {
           image: product.image,
           amount: products[i].amount
         })
-        productDocuments.push(await orderProduct)
+        productDocuments.push(orderProduct)
       }
       const order = await new global.models('Order')({
         products: productDocuments,
