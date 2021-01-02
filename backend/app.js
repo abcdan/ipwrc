7b0f09d3f828d2742bb14b187ee2f7090dfe1346
Daniel <daniel@lngzl.nl>
2021-01-02 20:55:13 +0100

    :construction: FIX CORS LATEr

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ var corsOptions = {
   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 }
 
-app.use(cors(corsOptions))
+app.use(cors()) // TODO: FIX CORS
 app.use('/uploads', express.static('uploads'))
 require('./database/init')()
 
