2fd8d113974aa81646fb87c177a27304ce2fd9c4
Daniel <daniel@lngzl.nl>
2021-01-03 13:30:06 +0100

    :sparkles: removing logs

--- backend/routes/product/create.js
+++ backend/routes/product/create.js
@@ -6,8 +6,6 @@ module.exports = (app, endpoint) => {
   * Create a new product
   */
   app.post(endpoint, authentication, async (req, res) => {
-    console.log(req.user.admin)
-    console.log(req.user)
     if (!req.user.admin) { return res.status(401).json({ success: false, message: 'you\'re not an admin' }) }
     const { slug, name, description, price, image } = req.body
     if (!slug || !name || !description || !price || !image) { return res.status(400).json({ success: false, message: 'you forgot one of the following slug, name, description, price, image' }) }
@@ -26,7 +24,6 @@ module.exports = (app, endpoint) => {
         return res.status(400).json({success: false, message: 'item already exists'})
       }
 
-      console.log(await findProduct)
       const product = new Product({
         slug: slugLowerCase,
         name,
--- backend/routes/user/upgrade-to-admin.js
+++ backend/routes/user/upgrade-to-admin.js
@@ -5,8 +5,6 @@ module.exports = (app, endpoint) => {
   // TODO: REQUIRED TOKEN
   app.post(endpoint, authentication, async (req, res) => {
     const { key } = req.body
-    console.log(key)
-    console.log(process.env.UPGRADE_KEY)
     if(!key || key !== process.env.UPGRADE_KEY) return res.status(401).json({ success: false, message: 'not the right key or no key provided'})
     try {
       const user = await User.findById(req.user.id)
